81a2deea9564538b13e1444e734261d6972ecc55
Chuck Fairy <charlieabeling@gmail.com>
2014-10-02 12:03:36 -0500

    Version 2.0.0 init

--- wilsonblock.js
+++ wilsonblock.js
@@ -1,146 +1,104 @@
 /**************WilsonBlock Div Carousel**************/
+var WILSON_THEMES = [];
+
 function WilsonBlock(windowId, classOn, classOff) {
 	"use strict";
 	//Check classes 
-    var classOn  = classOn  || "opacityOn";
-    var classOff = classOff || "opacityOff";  
-    
-    //Current Themes
-    this.themes = []; 
-    
-	//Find WilsonBlock folder
+    this.classOn  = classOn  || "opacityOn";
+    this.classOff = classOff || "opacityOff";  
+	
+	//Script location
 	var scripts = document.getElementsByTagName("script");
 	for(var i=0;i < scripts.length;i++) {
-		var scriptLocation = scripts[i].src.split("wilsonblock/").pop();
-		if(scriptLocation == "wilsonblock.js") {
-			this.serverLocation = scripts[i].src.split("wilsonblock/")[0] + "/wilsonblock/"; 
+		var scriptLocation = scripts[i].src.split("wilsonblock.js");
+		if(scriptLocation.length === 2) {
+			this.serverLocation = scriptLocation[0]; 
 		}
 	};
 	
-	//jQuery Load
-	if(typeof($()) != "undefined" ) {
-		var jqueryScript = document.createElement("script");
-		jqueryScript.src = this.serverLocation + "_assets/jquery-1.9.1.min.js";
-		document.head.appendChild(jqueryScript);
-	}
-	
 	//Find all slides. Labeled as div tags
-	this.window = document.getElementById(windowId);
-	this.slides = $(this.window).find("div");	
-	this.currentSlide = 0;	
+	this.window = document.getElementById(windowId);	
+	this.currentSlide = false;	
 	
-	//Clear All Slides
-	this.clearSlides = function() {
-		$(this.slides).each(function() {
-			if(this.display != "none") {
-				$(this).removeClass(classOn);
-				$(this).addClass(classOff);
-			}
-		})		
+	this.imgs = this.window.getElementsByTagName("img");
+	this.imgsHTML = document.createElement("div");
+	for(var t = 0; 0 < this.imgs.length; t++) {
+		var thisImg = this.imgs[this.imgs.length -1];
+		thisImg.setAttribute("data", t);
+		this.imgsHTML.appendChild(thisImg);
 	}
-	
-	//Show current slide.
-	this.showSlide = function() {
-		this.clearSlides();
-		$(this.slides[this.currentSlide]).removeClass(classOff);
-		$(this.slides[this.currentSlide]).addClass(classOn);
-		this.buttonSetup();
-	}
-	
-	//Next + Previous slide switcher//
-	this.nextSlide=function() {
-		if(this.currentSlide !== (this.slides.length-1)) {
-			this.currentSlide++;
-			this.showSlide();
-		}
-	}
-	this.prevSlide=function() {
-		if(this.currentSlide !== 0) {
-			this.currentSlide--;
-			this.showSlide();
-		}
-	}
-	
+
 	//Initialize the wilson
 	this.init();
 }
 
 //Set up the slides and Buttons
-WilsonBlock.prototype.init = function() {
-	this.buttonEnable();
-	this.buttonSetup();
-	this.clearSlides();
-	this.setTheme("wilsonbones");
-	this.showSlide();
-	return this;
-}
-
-//Sets up buttons display
-WilsonBlock.prototype.buttonSetup = function() {	
-	//Hide Non-usable
-	$(this.nextButton).css("display", "block");
-	$(this.prevButton).css("display", "block");
+WilsonBlock.prototype = {
+	init: function() {
+		this.layoutCreate();
+		this.previewSetup();
+		this.navSetup();
+		//this.clearSlides();
+		this.setTheme("wilsonbones");
+		this.setSlide(0);
+		return this;
+	},
 	
-	if(this.currentSlide == (this.slides.length-1)) {
-		$(this.nextButton).css("display", "none");
-	}	
-	if(this.currentSlide == 0) {
-		$(this.prevButton).css("display", "none");
-	}		
-}
+	layoutCreate: function() {
+		//Create main previewer
+		this.previewer = document.createElement("div");
+		this.window.innerHTML = null;
+		this.previewer.innerHTML = this.imgsHTML.innerHTML;
+		this.window.appendChild(this.previewer);
+		
+		this.navigation = document.createElement("div");
+		this.navigation.innerHTML = this.imgsHTML.innerHTML;
+		this.window.appendChild(this.navigation);
+	},
+	
+	previewSetup:function() {
+		var wilson = this;
+		for(var i = 0; i < this.previewer.childNodes.length;i++) {
+			var thisImg = this.previewer.childNodes[i];
+			thisImg.style.display = "none";
+			thisImg.animator = new animateHTML(thisImg, {
+				
+				classOn: wilson.classOn,
+				classOff: wilson.classOff
+			});
+			thisImg.animat
+		}	
+	},
+	
+	navSetup: function() {
+		var wilson = this;
+		addEvent("click", this.navigation, function(e) {
+			if(e.target.tagName !== "IMG") {return false;}
+			var index = e.target.getAttribute("data");
+			wilson.setSlide(index);		
+		});	
+	},
+	
+	setSlide: function(indexNumber) {
+		if(this.currentIndex === parseInt(indexNumber)) {return true;}
 
-//Create and Enable Buttons
-WilsonBlock.prototype.buttonEnable = function() {
-	this.prevButton  = $(this.window).find(".wilson-pre");
-	this.nextButton  = $(this.window).find(".wilson-next");
-	$(this.prevButton).on("click", $.proxy(this.prevSlide, this));
-	$(this.nextButton).on("click", $.proxy(this.nextSlide, this));
-}
+		if(this.currentSlide) {
+			this.currentSlide.animator.hide();
+		}
 
-
-/**************WilsonBlock API functions**************/
-
-//Set slide to specific slide number
-WilsonBlock.prototype.setSlide = function(slideNumber) {
-	if(parseInt(slideNumber) < (this.slides.length + 1)) {
-		this.currentSlide = parseInt(slideNumber)-1;
-		this.showSlide();
+		//Set new slide and show it
+		this.currentSlide = this.previewer.childNodes[indexNumber];	
+		this.currentSlide.animator.show();
+		this.currentIndex = parseInt(indexNumber);
+		
+		this.previewer.scrollTop = 0;	
 	}
-}
-
-//autoSlide flips through the slides and returns 1 after last
-WilsonBlock.prototype.autoSlide = function(timeOut) {
-	var e = this; //Using e because this isn't set in setInterval
-	setInterval(function() {
-		if(e.currentSlide == (e.slides.length-1)) {e.setSlide(1);}
-		else {e.nextSlide();}
-	} , parseInt(timeOut))
-}
-
-WilsonBlock.prototype.createPointers = function(preText, nextText){
-	preText = preText || "<";
-	nextText = nextText || ">";
-	
-	var pre  = document.createElement("span");
-	var next = document.createElement("span");
-	
-	pre.setAttribute("class", "wilson-pre");
-	next.setAttribute("class", "wilson-next");
-	
-	pre.innerHTML = preText;
-	next.innerHTML = nextText;
-	
-	this.window.appendChild(pre);
-	this.window.appendChild(next);
-	
-	this.buttonEnable();
-	this.buttonSetup();
 }
 
 /**************Set Theme**************/
 WilsonBlock.prototype.setTheme = function(theme) {
 	//Check if theme is loaded
-	if(!inArray(theme, this.themes)) {
+	if(!in_array(theme, WILSON_THEMES)) {
 		cssUrl = this.serverLocation + "_themes/" + theme + ".css";		
 		//Load and Append Css
 		if(typeof(cssUrl) !== "undefined") {
@@ -150,33 +108,103 @@ WilsonBlock.prototype.setTheme = function(theme) {
 			document.head.appendChild(wilsonCSS);
 		}
 		
-		this.themes.push(theme);
+		WILSON_THEMES.push(theme);
 	}
 	
-	$(this.window).addClass(theme);
+	this.window.className = theme;
+	this.previewer.className = theme + "-previewer";
+	this.navigation.className = theme + "-navigator";
 }
 
-//////Utils///////
 
-function inArray(needle, haystack) {
-	if(typeof(haystack) == "undefined") {return false;}
-	console.log(haystack);
-	for(var i=0; i < haystack.length;i++) {
-		console.log(haystack[i]);
-		if(haystack[i] == needle) {
-			return true;
-		}
+/**************Utils and Animation**************/
+function in_array(needle, haystack, argStrict) {
+	var key = '',
+	strict = !! argStrict;
+	if (strict) {
+		for(key in haystack) {if (haystack[key] === needle) {return true;}}
+	} else {
+		for(key in haystack) {if (haystack[key] == needle) {return true;}}
 	}
 	return false;
 }
 
+function addEvent(evnt, elem, func) {
+	if (elem.addEventListener) {
+		elem.addEventListener(evnt,func,false);
+	} else if (elem.attachEvent) { 
+			elem.attachEvent("on"+evnt, func);
+	} 
+	else { elem[evnt] = func;}
+}
 
+function animateHTML(animator, customize) {
+	if(typeof(animator) === "string") {this.animator = document.getElementById(animator);}
+	else if(typeof(animator) === "object") {
+		this.animator = animator;
+	} else {
+		console.log("Not object or string for animator.");
+		return false;	
+	}
+	
+	customize = customize || {};
+	
+	//Animation Data
+	this.classOn = customize.classOn || "";
+	this.classOff = customize.classOff || "";
+	this.aniTime = customize.animationTime || 750;
+	this.showDefault = customize.showDefault || false;
+	//Wrapper Data
+	this.isOn = false;
+		
+	this.init();
+}
 
-
-
-
-
-
-
-
-
+animateHTML.prototype = {
+	init: function() {
+		if(!this.showDefault) {this.clear();}	
+		else {
+			this.isOn = true;
+			this.animator.className = this.classOn;	
+			this.animator.style.display = "block";
+		}
+	},
+	
+	show: function() {
+		if(this.isOn) {return true;}
+		
+		this.animator.style.display = "block";
+		this.animator.style.opacity = "0";
+		
+		var thisOm = this;
+		setTimeout(function() {
+			thisOm.animator.className = thisOm.classOn;
+			thisOm.animator.style.opacity = "1";
+		}, 100);
+		this.isOn = true;
+	},
+	
+	hide: function() {
+		if(!this.isOn) {return true;}
+		
+		this.animator.className = this.classOff;
+		this.animator.style.opacity = "0";
+		
+		var thisOm = this;
+		setTimeout(function() {
+			thisOm.animator.style.display = "none";
+		}, thisOm.aniTime);
+		this.isOn = false;
+	},
+	
+	clear: function() {
+		this.animator.style.display = "none";
+		this.animator.className = this.classOff;
+		this.isOn = false;
+	},
+	
+	toggle: function() {
+		if(this.isOn) {this.hide();} 
+		else {this.show();}
+	}
+}
